5ac2188c235845e5732ff7f82230a420b0a10ed2
Indra Putra <indrakoslab@gmail.com>
2018-08-27 11:40:14 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+// this the link API
+import Link from 'next/link'
+import Unexpected_A from 'third-library'
+
+export default ({ href, name })=> (
+    
+    <Link href={href} passHref>
+        <Unexpected_A>
+            {name}
+        </Unexpected_A>
+    </Link>
+
+)
+
+    
